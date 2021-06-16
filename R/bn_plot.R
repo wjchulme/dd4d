4ede6c98994daf54124bb127b1d0d22b6a3e652c
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-06-16 15:54:51 +0100

    typo

--- R/bn_plot.R
+++ R/bn_plot.R
@@ -3,12 +3,12 @@
 #'
 #' @param bn_df initialised bn_df object, with simulation instructions. Created with `bn_create`
 #'
-#' @return
+#' @return plot
 #' @export
 #'
 #' @examples
 bn_plot <- function(bn_df){
   dagitty <- bn2dagitty(bn_df)
-  plot(daggity::graphLayout(dagitty))
+  plot(dagitty::graphLayout(dagitty))
 }
 
