0063e45c191d12fc0335dfe744dc2d446c13551d
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-06-16 16:53:44 +0100

    add rfactor function

--- R/rcat.R
+++ R/rcat.R
@@ -1,15 +1,22 @@
 #
-#' Random categorical variable
+#' Random categorical or factor variables
 #'
 #' @param n number of samples
 #' @param levels vector of categories to sample from
 #' @param p vector of probabilities
 #'
-#' @return
+#' @return a `character` or `factor` vector
 #' @export
 #'
 #' @examples
 #' #' rcat(n=10, levels=c("a","b"), p=c(0.2,0.8))
 rcat <- function(n, levels, p){
   sample(x=levels, size=n, replace=TRUE, prob=p)
+}
+
+#' @rdname rcat
+rfactor <- function(n, levels, p){
+  x <- sample(x=levels, size=n, replace=TRUE, prob=p)
+
+  factor(x = x, levels = levels)
 }
