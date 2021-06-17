64ae9c8f20c8c94d6504219ec8acd70e6a2b50ac
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-06-17 10:49:12 +0100

    option to only plot connected nodes

--- R/bn_plot.R
+++ R/bn_plot.R
@@ -2,13 +2,19 @@
 #' Plot bn_df object
 #'
 #' @param bn_df initialised bn_df object, with simulation instructions. Created with `bn_create`
+#' @param connected_only logical. Only plot nodes that are connected to other nodes
 #'
 #' @return plot
 #' @export
 #'
 #' @examples
-bn_plot <- function(bn_df){
-  dagitty <- bn2dagitty(bn_df)
+bn_plot <- function(bn_df, connected_only = FALSE){
+
+  if(connected_only){
+    dagitty <- bn2dagitty(bn_df[!(map_lgl(bn_df$parents, ~length(.)==0) & map_lgl(bn_df$children, ~length(.)==0)), ])
+  } else{
+    dagitty <- bn2dagitty(bn_df)
+  }
   plot(dagitty::graphLayout(dagitty))
 }
 
